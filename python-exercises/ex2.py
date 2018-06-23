be1b6ce6b8fead65dc14eff6877ac1aae40aaacc
MayThuHtun <maythutun.ucsmdy@gmail.com>
2018-06-23 11:45:13 -0700

    Learning comments and Printing

--- python-exercises/ex2.py
+++ python-exercises/ex2.py
@@ -1,13 +1,10 @@
-print("Hello World!")
+# A comment, this is so you can read your program later.
+#  Anything after the # is ignored by python.
 
-print("Hello Again")
+print("I could have code like this.")
 
-print("I like typing this.")
+# and the comment after is ignored
+#  You can also use a comment to "disable" or comment out code:
+#  print("This won't run.")
 
-print("This is fun.")
-
-print('Yay! Printing.')
-
-print("I'd much rather you 'not'.")
-
-print('I "said" do not touch this.')
+print("This will run.")
